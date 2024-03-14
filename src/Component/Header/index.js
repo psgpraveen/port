985cc6f7825690a0b187d0faa605a4bce61f5083
Psgpraveen <pg23403@gmail.com>
2024-03-14 17:31:12 +0530

    dfgg

--- src/Component/Header/index.js
+++ src/Component/Header/index.js
@@ -7,13 +7,12 @@ function index() {
 
 <nav className="bg-white border-gray-200 dark:bg-gray-900">
     <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl p-4">
-        <Link to="/" className="flex items-center cursor-pointer space-x-3 rtl:space-x-reverse">
+        <Link to="/port" className="flex items-center cursor-pointer space-x-3 rtl:space-x-reverse">
             <img src="https://flowbite.com/docs/images/logo.svg" className="h-8" alt="Flowbite Logo" />
             <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Psgpraveen</span>
         </Link>
         <div className="flex items-center space-x-6 rtl:space-x-reverse">
             <a href="tel:7985942726" className="text-sm  text-gray-500 dark:text-white hover:underline">+91 7985942726</a>
-            <a href="#" className="text-sm  text-blue-600 dark:text-blue-500 hover:underline">Login</a>
         </div>
     </div>
 </nav>
@@ -22,7 +21,7 @@ function index() {
         <div className="flex items-center">
             <ul className="flex flex-row font-medium mt-0 space-x-8 rtl:space-x-reverse text-sm">
                 <li>
-                    <a href="/" className="text-gray-900 dark:text-white hover:underline" aria-current="page">Home</a>
+                    <a href="/port" className="text-gray-900 dark:text-white hover:underline" aria-current="page">Home</a>
                 </li>
                 <li>
                     <Link to="/project" className="text-gray-900 dark:text-white hover:underline">Projects</Link>
